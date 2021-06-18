e5b030a85934813c78a7a65ef6914eb1e3953f8f
David Huemer <92david.huemer@gmail.com>
2021-06-18 21:22:15 +0200

    Added basic RadioTraining.

---
+++ RadioTrainingCreator.Data/RadioTraining.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RadioTrainingCreator.Data
+{
+    [Serializable]
+    public class RadioTraining
+    {
+        public string Author { get; set; } = "";
+        public string Comment { get; set; } = "";
+    }
+}

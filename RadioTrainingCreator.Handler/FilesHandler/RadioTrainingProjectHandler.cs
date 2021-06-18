ad5bea6947f62839c67997050b71c7b61d73a658
David Huemer <92david.huemer@gmail.com>
2021-06-18 22:53:57 +0200

    Added the RadioTrainingProjectHandler. Added the RadioTrainingProjectHandler_Tests.

---
+++ RadioTrainingCreator.Handler/FilesHandler/RadioTrainingProjectHandler.cs
@@ -0,0 +1,72 @@
+﻿using RadioTrainingCreator.Data;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RadioTrainingCreator.Handler.FilesHandler
+{
+    public static class RadioTrainingProjectHandler
+    {
+        #region Create
+
+        /// <summary>
+        /// Creates the radio training file
+        /// </summary>
+        /// <param name="folderPath">The path in that the file should be created</param>
+        /// <param name="radioTrainingName">The name of the radioTraining (without the file extension)</param>
+        /// <param name="author">The author of the radio training</param>
+        /// <param name="comment">The comment of the radioTraining</param>
+        /// <returns>The created RadioTraining</returns>
+        public static RadioTraining CreateRadioTraining(string folderPath, string radioTrainingName, 
+            string author, string comment)
+        {
+            CreeateDirectoryWhenRequired(folderPath);
+
+            string projectFile = $@"{Path.Combine(folderPath, radioTrainingName)}.fue";
+
+            if (File.Exists(projectFile))
+                throw new IOException($"The file {projectFile} is already existing");
+
+            var radioTraining = new RadioTraining
+            {
+                Name = radioTrainingName,
+                Author = author,
+                Comment = comment
+            };
+
+            SaveRadioTraining(projectFile, radioTraining);
+
+            return radioTraining;
+        }
+
+        /// <summary>
+        /// Creates the directory if the directory is not existing yet
+        /// </summary>
+        /// <param name="folder">The folder path that should be checked</param>
+        private static void CreeateDirectoryWhenRequired(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+
+        #endregion
+
+        #region Save
+
+        /// <summary>
+        /// Saves the RadioTraining object to a specific path
+        /// </summary>
+        /// <param name="filePath">The path where the RadioTraining should be saved</param>
+        /// <param name="radioTraining">The RadioTraining that should be saved</param>
+        public static void SaveRadioTraining(string filePath, RadioTraining radioTraining)
+        {
+            var formatter = new BinaryFormatter();
+            var stream = File.Open(filePath, FileMode.Create);
+            formatter.Serialize(stream, radioTraining);
+            stream.Close();
+        }
+
+        #endregion
+    }
+}
---
+++ RadioTrainingCreator.Tests/Handler/FilesHandler/RadioTrainingProjectHandler_Tests.cs
@@ -0,0 +1,54 @@
+﻿using RadioTrainingCreator.Handler.FilesHandler;
+using RadioTrainingCreator.Tests.Basics;
+using System.IO;
+using Xunit;
+
+namespace RadioTrainingCreator.Tests.Handler.FilesHandler
+{
+    public class RadioTrainingProjectHandler_Tests : BaseTest
+    {
+        //const string RADRIO_TRAINING_NAME = "testUebung"
+
+        /// <summary>
+        /// Checks if the RadioTrainingProjectHandler creates a folder when no folder is existing
+        /// </summary>
+        [Fact]
+        public void Create_Project_CreateFolder_Test()
+        {
+            string path = $"{TEST_ENVIRONMENT}NotExistingDirectory";
+
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+
+            RadioTrainingProjectHandler.CreateRadioTraining(path, "testUebung", "", "");
+            Assert.True(Directory.Exists(path));
+        }
+
+        /// <summary>
+        /// Checks if an IOExeption is thrown if the file is already existing
+        /// </summary>
+        [Fact]
+        public void Create_Project_FileExisting_Test()
+        {
+            string folderPath = $"{TEST_ENVIRONMENT}CreateNewProjectDirectory";
+            RequireExistingFile(folderPath, "uebung.fue");
+            Assert.Throws<IOException>(() => RadioTrainingProjectHandler.CreateRadioTraining(folderPath, "uebung", "", ""));
+        }
+
+        /// <summary>
+        /// Checks if the file is created
+        /// </summary>
+        [Fact]
+        public void Create_Project_FileCreated_Test()
+        {
+            string folderPath = $"{TEST_ENVIRONMENT}CreateNewProjectDirectory";
+            var filePath = RequireNotExistingFile(folderPath, "uebung.fue");
+            RadioTrainingProjectHandler.CreateRadioTraining(folderPath, "uebung", "", "");
+            Assert.True(File.Exists(filePath));
+        }
+
+
+    }
+}

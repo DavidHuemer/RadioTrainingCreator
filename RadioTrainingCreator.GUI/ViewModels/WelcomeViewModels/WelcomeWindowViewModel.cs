3f9a1295710a4cc085c3758d43d586b57237a251
David Huemer <92david.huemer@gmail.com>
2021-06-18 12:06:41 +0200

    Added WelcomeViewModel and WelcomeWindow.

---
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels
+{
+    public class WelcomeWindowViewModel : WindowViewModel
+    {
+        public WelcomeWindowViewModel() : base("Wilkommen zum Funkübung Ersteller")
+        {
+
+        }
+    }
+}

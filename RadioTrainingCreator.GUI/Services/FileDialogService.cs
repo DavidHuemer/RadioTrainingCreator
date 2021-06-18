d937fec8c94b618af235740054ae6eb8af872b7b
David Huemer <92david.huemer@gmail.com>
2021-06-18 13:57:04 +0200

    Added FileDialogService.

---
+++ RadioTrainingCreator.GUI/Services/FileDialogService.cs
@@ -0,0 +1,20 @@
+﻿using RadioTrainingCreator.GUI.Services.Interfaces;
+using System.Windows.Forms;
+
+namespace RadioTrainingCreator.GUI.Services
+{
+    public class FileDialogService : IFileDialogService
+    {
+        public string GetFolder()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    return fbd.SelectedPath;
+
+                return "";
+            }
+        }
+    }
+}

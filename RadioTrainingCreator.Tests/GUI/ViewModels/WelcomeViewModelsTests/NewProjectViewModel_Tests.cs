4d2594161607d31531239bc88947c9aa483db18c
David Huemer <92david.huemer@gmail.com>
2021-06-18 15:09:56 +0200

    Added NewProjectViewModel_Tests.

---
+++ RadioTrainingCreator.Tests/GUI/ViewModels/WelcomeViewModelsTests/NewProjectViewModel_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using RadioTrainingCreator.GUI.Services.Interfaces;
+using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels;
+using Xunit;
+
+namespace RadioTrainingCreator.Tests.GUI.ViewModels.WelcomeViewModelsTests
+{
+    public class NewProjectViewModel_Tests
+    {
+        #region GetFolder
+
+        [Theory]
+        [InlineData(@"F:\Feuerwehr")]
+        [InlineData(@"F:\Feuerwehr\")]
+        [InlineData(@"")]
+        public void GetFolder_Tests(string expected)
+        {
+            var mock = new Mock<IFileDialogService>();
+            mock.Setup(service => service.GetFolder()).Returns(expected);
+
+            var newProjectViewModel = new NewProjectViewModel(mock.Object);
+            newProjectViewModel.DoChooseFolder();
+            Assert.Equal(expected, newProjectViewModel.ProjectFolder);
+        }
+
+        #endregion
+    }
+}

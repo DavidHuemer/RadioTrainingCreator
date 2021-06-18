79cf1c637ffe5e5941d107131663987323d886ad
David Huemer <92david.huemer@gmail.com>
2021-06-18 22:53:25 +0200

    Added the BaseTest

---
+++ RadioTrainingCreator.Tests/Basics/BaseTest.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace RadioTrainingCreator.Tests.Basics
+{
+    public abstract class BaseTest
+    {
+        /// <summary>
+        /// The base path of all files that work with paths
+        /// </summary>
+        public const string TEST_ENVIRONMENT = @"F:\Feuerwehr\Testing\";
+
+        /// <summary>
+        /// Makes sure that the folder and the file exists
+        /// </summary>
+        /// <param name="folder">The folder that must exist</param>
+        /// <param name="fileName">The fileName that must exist in the folder</param>
+        /// <returns>The path of the file that must exist</returns>
+        protected string RequireExistingFile(string folder, string fileName)
+        {
+            RequireDirectory(folder);
+
+            string filePath = Path.Combine(folder, fileName);
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+
+            return filePath;
+        }
+
+        /// <summary>
+        /// Makes sure that the folder exists but not the file
+        /// </summary>
+        /// <param name="folder">The folder that must exist</param>
+        /// <param name="fileName">The fileName that should not exist</param>
+        /// <returns>The path of the file that can be created but that is not existing yet</returns>
+        protected string RequireNotExistingFile(string folder, string fileName)
+        {
+            RequireDirectory(folder);
+
+            string filePath = Path.Combine(folder, fileName);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            return filePath;
+        }
+
+        /// <summary>
+        /// Makes sure the folder exists
+        /// </summary>
+        /// <param name="folder">The folder that must exist</param>
+        private void RequireDirectory(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+    }
+}

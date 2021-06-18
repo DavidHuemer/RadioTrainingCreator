59966498fb4678df513828c943b581e051049e6c
David Huemer <92david.huemer@gmail.com>
2021-06-18 22:52:14 +0200

    Added the name to the RadioTraining

--- RadioTrainingCreator.Data/RadioTraining.cs
+++ RadioTrainingCreator.Data/RadioTraining.cs
@@ -5,6 +5,7 @@ namespace RadioTrainingCreator.Data
     [Serializable]
     public class RadioTraining
     {
+        public string Name { get; set; } = "";
         public string Author { get; set; } = "";
         public string Comment { get; set; } = "";
     }

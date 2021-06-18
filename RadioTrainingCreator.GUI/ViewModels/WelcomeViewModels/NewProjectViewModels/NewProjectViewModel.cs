2ee74faa30b027a49740f499ae0351c8a42404ce
David Huemer <92david.huemer@gmail.com>
2021-06-18 13:15:42 +0200

    Added NewProjectViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/NewProjectViewModels/NewProjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels
+{
+    public class NewProjectViewModel : BaseViewModel
+    {
+        public string ProjectName { get; set; } = "";
+        public string ProjectFolder { get; set; } = "";
+        public string FullPath { get; set; } = "";
+        public string Author { get; set; } = "";
+        public string Comment { get; set; } = "";
+    }
+}
--- RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels;
 
 namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels
 {
     public class WelcomeWindowViewModel : WindowViewModel
     {
+        public NewProjectViewModel NewProjectViewModel { get; set; }
+
         public WelcomeWindowViewModel() : base("Wilkommen zum Funkübung Ersteller")
         {
-
+            NewProjectViewModel = new NewProjectViewModel();
         }
     }
 }

d56a9a55a3b10ca7b299fe038719ee4d98d36102
David Huemer <92david.huemer@gmail.com>
2021-06-18 11:50:37 +0200

    Added BaseViewModel

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
+{
+    /// <summary>
+    /// Basic class for all ViewModels
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Is called when a property has changed
+        /// </summary>
+        /// <param name="name">The name of the changed property</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}

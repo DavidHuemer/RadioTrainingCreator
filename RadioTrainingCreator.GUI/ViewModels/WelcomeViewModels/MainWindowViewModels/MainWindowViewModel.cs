36328eac1d0c40d438ad66458b99cb88d17ed6df
David Huemer <92david.huemer@gmail.com>
2021-06-19 18:34:27 +0200

    Added MainWindowViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/MainWindowViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.MainWindowViewModels
+{
+    public class MainWindowViewModel : WindowViewModel
+    {
+        public MainWindowViewModel() : base("RadioTrainingCreator")
+        {
+
+        }
+    }
+}

4d2e3875c0b9631b76204d71c84c08e126a9323f
David Huemer <92david.huemer@gmail.com>
2021-06-20 21:10:05 +0200

    Added RecentlyOpenedFilesHandler in the OpenProjectViewModel.

--- RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/OpenProjectViewModels/OpenProjectViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/OpenProjectViewModels/OpenProjectViewModel.cs
@@ -1,9 +1,20 @@
 ﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+using RadioTrainingCreator.Handler.FilesHandler;
+using RadioTrainingCreator.Handler.Services.Interfaces.FileInterfaces;
+using RadioTrainingCreator.Handler.Services.Services.FileServices;
 
 namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.OpenProjectViewModels
 {
     public class OpenProjectViewModel : BaseViewModel
     {
+        private RecentlyOpenedFilesHandler RecentlyOpenedFilesHandler;
 
+        public OpenProjectViewModel(IRecentlyOpenedFilesService recentlyOpenedFilesService = null)
+        {
+            if (recentlyOpenedFilesService == null)
+                recentlyOpenedFilesService = new RecentlyOpenedFilesService();
+
+            RecentlyOpenedFilesHandler = new RecentlyOpenedFilesHandler(recentlyOpenedFilesService);
+        }
     }
 }

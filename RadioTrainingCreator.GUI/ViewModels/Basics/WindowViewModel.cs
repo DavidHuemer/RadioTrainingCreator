0e2b155ca69980905e2ee550a7192e2532c0bb65
David Huemer <92david.huemer@gmail.com>
2021-06-20 23:03:08 +0200

    Added close functionality to the WindowViewModel.

--- RadioTrainingCreator.GUI/ViewModels/Basics/WindowViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/Basics/WindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+﻿using System;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
 {
     public abstract class WindowViewModel : BaseViewModel
     {
@@ -7,6 +9,28 @@
         public WindowViewModel(string title)
         {
             Title = title;
+        }
+
+        /// <summary>
+        /// Will be called when the viewModel wants to close the window
+        /// </summary>
+        private Action close;
+
+        /// <summary>
+        /// Inits the WindowViewModel
+        /// </summary>
+        /// <param name="close">The action that will be called when the viewmodel wants to close the window</param>
+        public void Init(Action close)
+        {
+            this.close = close;
+        }
+
+        /// <summary>
+        /// Closes the window
+        /// </summary>
+        protected void CloseWindow()
+        {
+            close?.Invoke();
         }
     }
 }

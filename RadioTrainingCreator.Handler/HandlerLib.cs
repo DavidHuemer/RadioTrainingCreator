82af227702ba4e34a6190a1f1080df2d22c5cc76
David Huemer <92david.huemer@gmail.com>
2021-06-20 22:37:58 +0200

    Added HandlerLib

---
+++ RadioTrainingCreator.Handler/HandlerLib.cs
@@ -0,0 +1,24 @@
+﻿namespace RadioTrainingCreator.Handler
+{
+    /// <summary>
+    /// Contains handlers for the entire application
+    /// </summary>
+    public class HandlerLib
+    {
+        #region Singleton
+
+        private static HandlerLib instance = null;
+        public static HandlerLib Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new HandlerLib();
+
+                return instance;
+            }
+        }
+
+        #endregion
+    }
+}

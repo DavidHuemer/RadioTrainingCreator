ca09e398b84571fa9b0d3b9e418a4066f883fe0e
David Huemer <92david.huemer@gmail.com>
2021-06-20 17:11:26 +0200

    Added RecentlyOpenedFilesService.

---
+++ RadioTrainingCreator.Handler/Services/Services/FileServices/RecentlyOpenedFilesService.cs
@@ -0,0 +1,17 @@
+﻿using RadioTrainingCreator.Handler.Services.Interfaces.FileInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioTrainingCreator.Handler.Services.Services.FileServices
+{
+    public class RecentlyOpenedFilesService : IRecentlyOpenedFilesService
+    {
+        public void AddOrUpdateProject(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

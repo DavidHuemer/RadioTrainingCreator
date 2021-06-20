3f7df54eb51e7e9df45fc3b8e4e99d013fbcfb21
David Huemer <92david.huemer@gmail.com>
2021-06-20 13:27:30 +0200

    Added ConsoleMessageService.

---
+++ RadioTrainingCreator.GUI/Services/Services/MessageServices/ConsoleMessageService.cs
@@ -0,0 +1,23 @@
+﻿using RadioTrainingCreator.GUI.Services.Interfaces;
+using System;
+
+namespace RadioTrainingCreator.GUI.Services.Services.MessageServices
+{
+    public class ConsoleMessageService : IMessageService
+    {
+        public void Show(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public void ShowError(string title, string error)
+        {
+            Console.WriteLine($"ERROR: {error}");
+        }
+
+        public void ShowWarning(string title, string warning)
+        {
+            Console.WriteLine($"WARNING: {warning}");
+        }
+    }
+}

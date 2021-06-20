163b8d3fdf19a8eea09906d5e918f66a40fb0f6e
David Huemer <92david.huemer@gmail.com>
2021-06-20 13:08:37 +0200

    Added OpenProjectViewModel to WelcomeWindowViewModel

--- RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowViewModel.cs
@@ -1,16 +1,19 @@
 ﻿using RadioTrainingCreator.GUI.Services;
 using RadioTrainingCreator.GUI.ViewModels.Basics;
 using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels;
+using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.OpenProjectViewModels;
 
 namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels
 {
     public class WelcomeWindowViewModel : WindowViewModel
     {
         public NewProjectViewModel NewProjectViewModel { get; set; }
+        public OpenProjectViewModel OpenProjectViewModel { get; set; }
 
         public WelcomeWindowViewModel() : base("Wilkommen zum Funkübung Ersteller")
         {
             NewProjectViewModel = new NewProjectViewModel(new FileDialogService());
+            OpenProjectViewModel = new OpenProjectViewModel();
         }
     }
 }

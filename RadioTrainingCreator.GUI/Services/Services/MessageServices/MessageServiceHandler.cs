91388d286a7bed6b261904a11c89c5db4fe13c7d
David Huemer <92david.huemer@gmail.com>
2021-06-20 14:45:42 +0200

    Added MessageServiceHandler. Added MessageServiceHandler_Tests.

---
+++ RadioTrainingCreator.GUI/Services/Services/MessageServices/MessageServiceHandler.cs
@@ -0,0 +1,40 @@
+﻿using RadioTrainingCreator.GUI.Services.Interfaces;
+using RadioTrainingCreator.Handler.Diagnostics;
+
+namespace RadioTrainingCreator.GUI.Services.Services.MessageServices
+{
+    /// <summary>
+    /// Handles MessageService operations
+    /// </summary>
+    public static class MessageServiceHandler
+    {
+        /// <summary>
+        /// Returns the correct MessageService depending on the environment
+        /// </summary>
+        /// <returns>Correct MessageService depending on the environment</returns>
+        public static IMessageService GetCorrectMessageService()
+        {
+            bool isTestEnvironment = EnvironmentDetection.IsTestEnvironment();
+            return GetCorrectMessageService(isTestEnvironment);
+        }
+
+        /// <summary>
+        /// Returns the correct MessageService depending on the environment
+        /// </summary>
+        /// <param name="isTestEnvironment">Should be true when the environment is a test environment 
+        /// and false when the application runs in normal mode</param>
+        /// <returns>Correct MessageService depending on the environment</returns>
+        public static IMessageService GetCorrectMessageService(bool isTestEnvironment)
+        {
+            if (isTestEnvironment)
+            {
+                return new ConsoleMessageService();
+            }
+            else
+            {
+                return new MessageBoxMessageService();
+            }
+        }
+    }
+}
+ 
---
+++ RadioTrainingCreator.Tests/GUI/Services/MessageServiceHandler_Tests.cs
@@ -0,0 +1,42 @@
+﻿using RadioTrainingCreator.GUI.Services.Services.MessageServices;
+using Xunit;
+
+namespace RadioTrainingCreator.Tests.GUI.Services
+{
+    public class MessageServiceHandler_Tests
+    {
+        #region GetCorrectMessageService
+
+        /// <summary>
+        /// Checks if the MessageServiceHandler returns the test-environment service
+        /// </summary>
+        [Fact]
+        public void ReturnConsoleService_Test()
+        {
+            var service = MessageServiceHandler.GetCorrectMessageService();
+            Assert.True(service is ConsoleMessageService);
+        }
+
+        /// <summary>
+        /// Checks if the MessageServiceHandler returns the MessageBoxService
+        /// </summary>
+        [Fact]
+        public void ReturnMessageBoxService_ByVariable_Test()
+        {
+            var service = MessageServiceHandler.GetCorrectMessageService(false);
+            Assert.True(service is MessageBoxMessageService);
+        }
+
+        /// <summary>
+        /// Checks if the MessageServiceHandler returns the ConsoleMessageService
+        /// </summary>
+        [Fact]
+        public void ReturnConsoleService_ByVariable_Test()
+        {
+            var service = MessageServiceHandler.GetCorrectMessageService(true);
+            Assert.True(service is ConsoleMessageService);
+        }
+
+        #endregion
+    }
+}

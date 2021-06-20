e8e11ade6fa121c0c1d5d5af73d9a771dc239086
David Huemer <92david.huemer@gmail.com>
2021-06-20 17:21:48 +0200

    Added RecentlyOpenedFilesHandler

---
+++ RadioTrainingCreator.Handler/FilesHandler/RecentlyOpenedFilesHandler.cs
@@ -0,0 +1,14 @@
+﻿using RadioTrainingCreator.Handler.Services.Interfaces.FileInterfaces;
+
+namespace RadioTrainingCreator.Handler.FilesHandler
+{
+    public class RecentlyOpenedFilesHandler
+    {
+        private IRecentlyOpenedFilesService recentlyOpenedFilesService;
+
+        public RecentlyOpenedFilesHandler(IRecentlyOpenedFilesService recentlyOpenedFilesService)
+        {
+            this.recentlyOpenedFilesService = recentlyOpenedFilesService;
+        }
+    }
+}

d7974649da5a0026053a47dcd11e45e54d8a18e1
David Huemer <92david.huemer@gmail.com>
2021-06-20 14:28:48 +0200

    Added EnvironmentDetection. Added EnvironmentDetection_Tests.

---
+++ RadioTrainingCreator.Handler/Diagnostics/EnvironmentDetection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace RadioTrainingCreator.Handler.Diagnostics
+{
+    /// <summary>
+    /// Handles environment detections
+    /// </summary>
+    public static class EnvironmentDetection
+    {
+        /// <summary>
+        /// Checks if the application is running in testMode
+        /// </summary>
+        /// <returns>If the application is running in testMode</returns>
+        public static bool IsTestEnvironment()
+        {
+            string testAssemblyName = "xunit";
+            var isUnitTesting = AppDomain.CurrentDomain.GetAssemblies()
+                .Any(a => a.FullName.StartsWith(testAssemblyName));
+
+            return isUnitTesting;
+        }
+        }
+    }
+}
---
+++ RadioTrainingCreator.Tests/Handler/Diagnostics/EnvironmentDetection_Tests.cs
@@ -0,0 +1,17 @@
+﻿using RadioTrainingCreator.Handler.Diagnostics;
+using Xunit;
+
+namespace RadioTrainingCreator.Tests.Handler.Diagnostics
+{
+    public class EnvironmentDetection_Tests
+    {
+        /// <summary>
+        /// Checks if the environment is a testing environment
+        /// </summary>
+        [Fact]
+        public void IsTestEnvironment_Test()
+        {
+            Assert.True(EnvironmentDetection.IsTestEnvironment());
+        }
+    }
+}

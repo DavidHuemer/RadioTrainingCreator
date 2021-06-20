e6b96fe902d99d503b3fcee34de65e77ab3448ca
David Huemer <92david.huemer@gmail.com>
2021-06-20 13:22:21 +0200

    Added MessageBoxMessageService.

---
+++ RadioTrainingCreator.GUI/Services/Services/MessageServices/MessageBoxMessageService.cs
@@ -0,0 +1,23 @@
+﻿using RadioTrainingCreator.GUI.Services.Interfaces;
+using System.Windows;
+
+namespace RadioTrainingCreator.GUI.Services.Services.MessageServices
+{
+    public class MessageBoxMessageService : IMessageService
+    {
+        public void Show(string text)
+        {
+            MessageBox.Show(text);
+        }
+
+        public void ShowError(string title, string error)
+        {
+            MessageBox.Show(error, title, MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.No);
+        }
+
+        public void ShowWarning(string title, string warning)
+        {
+            MessageBox.Show(warning, title, MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.No);
+        }
+    }
+}

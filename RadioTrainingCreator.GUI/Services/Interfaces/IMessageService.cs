c145274dac74d8d44e704dde7a102105ee2cdfad
David Huemer <92david.huemer@gmail.com>
2021-06-20 13:20:16 +0200

    Added IMessageService

---
+++ RadioTrainingCreator.GUI/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,11 @@
+﻿namespace RadioTrainingCreator.GUI.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        void Show(string text);
+
+        void ShowWarning(string title, string warning);
+
+        void ShowError(string title, string error);
+    }
+}

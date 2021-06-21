0a40e2a951e16745fdfbc48a2c3a1781bec065b9
David Huemer <92david.huemer@gmail.com>
2021-06-21 21:50:45 +0200

    Added DataViewModel support in RadioTrainingDataWindowViewModel.

--- RadioTrainingCreator.GUI/ViewModels/DataWindowsViewModels/RadioTrainingDataWindowViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/DataWindowsViewModels/RadioTrainingDataWindowViewModel.cs
@@ -1,12 +1,25 @@
-﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+﻿using RadioTrainingCreator.Data.Files;
+using RadioTrainingCreator.GUI.Services.Interfaces.FileInterfaces;
+using RadioTrainingCreator.GUI.ViewModels.Basics;
+using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels;
+using System;
+using System.ComponentModel;
 
 namespace RadioTrainingCreator.GUI.ViewModels.DataWindowsViewModels
 {
-    public class RadioTrainingDataWindowViewModel : DataWindowViewModel
+    public class RadioTrainingDataWindowViewModel : DataWindowViewModel<CreatedRadioTraining>
     {
-        public RadioTrainingDataWindowViewModel() : base("Neue Funkübung erstellen")
-        {
+        public NewProjectDataViewModel DataViewModel { get; set; }
 
+        public RadioTrainingDataWindowViewModel(IFileDialogService fileDialogService) : base("Neue Funkübung erstellen")
+        {
+            DataViewModel = new NewProjectDataViewModel(fileDialogService);
+            DataViewModel.Init(ProjectCreated);
+        }
+
+        private void ProjectCreated(CreatedRadioTraining createdRadioTraining)
+        {
+            ReturnData(createdRadioTraining);
         }
     }
 }

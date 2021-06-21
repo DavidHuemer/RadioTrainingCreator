e2d4ba6ef0e8d304dde7f638df5e24c417a4a427
David Huemer <92david.huemer@gmail.com>
2021-06-21 20:58:08 +0200

    Moved most tests to the NewProjectDataViewModel_Tests. Added the test that the project created callback must be called.

--- RadioTrainingCreator.Tests/GUI/ViewModels/WelcomeViewModelsTests/NewProjectViewModel_Tests.cs
+++ RadioTrainingCreator.Tests/GUI/ViewModels/WelcomeViewModelsTests/NewProjectViewModel_Tests.cs
@@ -3,129 +3,39 @@ using RadioTrainingCreator.Data;
 using RadioTrainingCreator.GUI.Services.Interfaces.FileInterfaces;
 using RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels;
 using RadioTrainingCreator.Tests.Basics;
+using System;
 using Xunit;
 
 namespace RadioTrainingCreator.Tests.GUI.ViewModels.WelcomeViewModelsTests
 {
     public class NewProjectViewModel_Tests : BaseTest
     {
-        #region UpdateFullPath
+        private bool callbackCalled = false;
 
-        [Theory]
-        [InlineData(@"C:\Feuerwehr", "Uebung", "Uebung.fue", @"C:\Feuerwehr\Uebung.fue")]
-        [InlineData(@"C:\Feuerwehr\", "Uebung", "Uebung.fue", @"C:\Feuerwehr\Uebung.fue")]
-        [InlineData(@"C:\Feuerwehr", "", "", @"C:\Feuerwehr")]
-        [InlineData(@"C:\Feuerwehr\", "", "", @"C:\Feuerwehr")]
-        [InlineData(@"", "Uebung", "Uebung.fue", @"Uebung.fue")]
-        public void UpdateFullPath_Tests(string folder, string name, string nameExpected, string expected)
-        {
-            var mock = new Mock<IFileDialogService>();
-            var vm = new NewProjectViewModel(mock.Object)
-            {
-                ProjectName = name
-            };
-            Assert.Equal(nameExpected, vm.FullPath);
-            vm.ProjectFolder = folder;
-            Assert.Equal(expected, vm.FullPath);
-        }
-
-        [Theory]
-        [InlineData(@"C:\Feuerwehr", "Uebung", @"C:\Feuerwehr", @"C:\Feuerwehr\Uebung.fue")]
-        [InlineData(@"C:\Feuerwehr\", "Uebung", @"C:\Feuerwehr", @"C:\Feuerwehr\Uebung.fue")]
-        [InlineData(@"C:\Feuerwehr", "", @"C:\Feuerwehr", @"C:\Feuerwehr")]
-        [InlineData(@"C:\Feuerwehr\", "", @"C:\Feuerwehr", @"C:\Feuerwehr")]
-        [InlineData(@"", "Uebung", "", @"Uebung.fue")]
-        public void UpdateFullPath_FolderFirst_Tests(string folder, string name, string folderExpected, string fullExpected)
-        {
-            var mock = new Mock<IFileDialogService>();
-            var vm = new NewProjectViewModel(mock.Object)
-            {
-                ProjectFolder = folder
-            };
-            Assert.Equal(folderExpected, vm.FullPath);
-            vm.ProjectName = name;
-            Assert.Equal(fullExpected, vm.FullPath);
-        }
-
-        #endregion
-
-        #region GetFolder
-
-        [Theory]
-        [InlineData(@"F:\Feuerwehr")]
-        [InlineData(@"F:\Feuerwehr\")]
-        [InlineData(@"")]
-        public void GetFolder_Tests(string expected)
-        {
-            var mock = new Mock<IFileDialogService>();
-            mock.Setup(service => service.GetFolder()).Returns(expected);
-
-            var newProjectViewModel = new NewProjectViewModel(mock.Object);
-            newProjectViewModel.DoChooseFolder();
-            Assert.Equal(expected, newProjectViewModel.ProjectFolder);
-        }
-
-        #endregion
-
-        #region CurrentOpenedProject
-
-        /// <summary>
-        /// Checks if the current opened project is the created project
-        /// </summary>
         [Fact]
-        public void CurrentOpenedProject_Test()
+        public void Check_Callback()
         {
             var mock = new Mock<IFileDialogService>();
-            mock.Setup(service => service.GetFolder()).Returns("");
+            var newProjectVm = new NewProjectViewModel(mock.Object);
+            newProjectVm.Init(ProjectCreatedCallback);
 
             string folder = $"{TEST_ENVIRONMENT}";
             string name = $"uebung.fue";
 
             RequireNotExistingFile(folder, name);
 
-            var newProjectViewModel = new NewProjectViewModel(mock.Object)
-            {
-                ProjectName = "uebung",
-                ProjectFolder = folder,
-                Author = "Max",
-                Comment = "ExampleComment"
-            };
+            newProjectVm.DataViewModel.ProjectName = "uebung";
+            newProjectVm.DataViewModel.ProjectFolder = folder;
+            newProjectVm.DataViewModel.Author = "Max";
+            newProjectVm.DataViewModel.Comment = "ExampleComment";
 
-            newProjectViewModel.DoCreateRadioTraining();
-            //CurrentOpenedProject.Instance.RadioTraining.Name ==
-            Assert.Equal("uebung", CurrentOpenedProject.Instance.RadioTraining.Name);
-            Assert.Equal("Max", CurrentOpenedProject.Instance.RadioTraining.Author);
-            Assert.Equal("ExampleComment", CurrentOpenedProject.Instance.RadioTraining.Comment);
+            newProjectVm.DataViewModel.DoCreateRadioTraining();
+            Assert.True(callbackCalled);
         }
 
-        /// <summary>
-        /// Checks if the current opened filePath is correct
-        /// </summary>
-        [Fact]
-        public void CurrenttProjectFilePath_Test()
+        private void ProjectCreatedCallback()
         {
-            var mock = new Mock<IFileDialogService>();
-            mock.Setup(service => service.GetFolder()).Returns("");
-
-            string folder = $"{TEST_ENVIRONMENT}";
-            string name = $"uebung.fue";
-
-            RequireNotExistingFile(folder, name);
-
-            var newProjectViewModel = new NewProjectViewModel(mock.Object)
-            {
-                ProjectName = "uebung",
-                ProjectFolder = folder,
-                Author = "Max",
-                Comment = "ExampleComment"
-            };
-
-            string fullPath = $"{folder}{name}";
-
-            newProjectViewModel.DoCreateRadioTraining();
-            Assert.Equal(fullPath, CurrentOpenedProject.Instance.OpenedProjectFile);
+            callbackCalled = true;
         }
-
-        #endregion
     }
 }

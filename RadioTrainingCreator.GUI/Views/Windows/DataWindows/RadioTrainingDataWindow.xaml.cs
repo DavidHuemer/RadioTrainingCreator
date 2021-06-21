7828468861b8551dac75795759db1261882af0f1
David Huemer <92david.huemer@gmail.com>
2021-06-21 21:56:26 +0200

    Added RadioTrainingDataWindow.

---
+++ RadioTrainingCreator.GUI/Views/Windows/DataWindows/RadioTrainingDataWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.DataWindowsViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RadioTrainingCreator.GUI.Views.Windows.DataWindows
+{
+    /// <summary>
+    /// Interaktionslogik für RadioTrainingDataWindow.xaml
+    /// </summary>
+    public partial class RadioTrainingDataWindow : Window
+    {
+        public RadioTrainingDataWindowViewModel DataViewModel { get; set; }
+
+        public RadioTrainingDataWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetUpDataContext(RadioTrainingDataWindowViewModel radioTrainingWindow)
+        {
+            DataViewModel = radioTrainingWindow;
+            DataContext = radioTrainingWindow;
+            Closing += radioTrainingWindow.Closing;
+        }
+    }
+}

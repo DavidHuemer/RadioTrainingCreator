fc40cf76577a8b44810de4a90114bea70d95322a
David Huemer <92david.huemer@gmail.com>
2021-06-21 17:16:40 +0200

    Added DataWindowViewModel. Added RadioTrainingDataWindowViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/DataWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+{
+    public abstract class DataWindowViewModel : WindowViewModel
+    {
+        public DataWindowViewModel(string title) : base(title)
+        {
+
+        }
+    }
+}
---
+++ RadioTrainingCreator.GUI/ViewModels/DataWindowsViewModels/RadioTrainingDataWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.DataWindowsViewModels
+{
+    public class RadioTrainingDataWindowViewModel : DataWindowViewModel
+    {
+        public RadioTrainingDataWindowViewModel() : base("Neue Funkübung erstellen")
+        {
+
+        }
+    }
+}

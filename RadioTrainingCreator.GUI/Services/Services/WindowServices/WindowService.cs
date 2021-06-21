2ae97a4a21341a42470102d8d67587725ff41a2f
David Huemer <92david.huemer@gmail.com>
2021-06-21 21:49:44 +0200

    Added RadioTrainingDataWindow support.

--- RadioTrainingCreator.GUI/Services/Services/WindowServices/WindowService.cs
+++ RadioTrainingCreator.GUI/Services/Services/WindowServices/WindowService.cs
@@ -1,7 +1,9 @@
 ﻿using RadioTrainingCreator.GUI.Services.Interfaces;
 using RadioTrainingCreator.GUI.ViewModels.Basics;
+using RadioTrainingCreator.GUI.ViewModels.DataWindowsViewModels;
 using RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels;
 using RadioTrainingCreator.GUI.Views.Windows;
+using RadioTrainingCreator.GUI.Views.Windows.DataWindows;
 
 namespace RadioTrainingCreator.GUI.Services.Services.WindowServices
 {
@@ -13,12 +15,24 @@ namespace RadioTrainingCreator.GUI.Services.Services.WindowServices
             {
                 OpenMainWindowViewModel(viewModel as MainWindowViewModel);
             }
+
+            if(viewModel is RadioTrainingDataWindowViewModel)
+            {
+                ShowRadioTrainingDataWindowViewModel(viewModel as RadioTrainingDataWindowViewModel);
+            }
         }
 
         private void OpenMainWindowViewModel(MainWindowViewModel mainWindowViewModel)
         {
             MainWindow.Instance.Show();
             //MainWindow.Instance.DataContext = mainWindowViewModel;
+        }
+
+        private void ShowRadioTrainingDataWindowViewModel(RadioTrainingDataWindowViewModel radioTrainingWindow)
+        {
+            var window = new RadioTrainingDataWindow();
+            window.SetUpDataContext(radioTrainingWindow);
+            window.ShowDialog();
         }
     }
 }

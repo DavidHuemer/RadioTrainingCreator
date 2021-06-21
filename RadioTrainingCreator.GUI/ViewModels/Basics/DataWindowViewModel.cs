686ff8d1a4965919ee41e67111341bd8fc082258
David Huemer <92david.huemer@gmail.com>
2021-06-21 21:34:58 +0200

    Added in the DataWindowViewModel: HasReturned, Init, ReturnData, Closing.

--- RadioTrainingCreator.GUI/ViewModels/Basics/DataWindowViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/Basics/DataWindowViewModel.cs
@@ -1,10 +1,50 @@
-﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+﻿using System.ComponentModel;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
 {
-    public abstract class DataWindowViewModel : WindowViewModel
+    public abstract class DataWindowViewModel<T> : WindowViewModel where T : class
     {
+        public delegate void ReturnDataDel(T data);
+        private ReturnDataDel returnDataDel;
+
+        /// <summary>
+        /// Defines if something has returned
+        /// </summary>
+        protected bool hasReturned;
+
         public DataWindowViewModel(string title) : base(title)
         {
 
+        }
+
+        /// <summary>
+        /// Inits the DataWindow
+        /// </summary>
+        /// <param name="del">The callback that is called when the viewModel wants to return something</param>
+        public void Init(ReturnDataDel del)
+        {
+            returnDataDel = del;
+        }
+
+        /// <summary>
+        /// Is called when the viewModel wants to return something
+        /// </summary>
+        /// <param name="data">The returning data</param>
+        protected void ReturnData(T data)
+        {
+            returnDataDel?.Invoke(data);
+            CloseWindow();
+        }
+
+        /// <summary>
+        /// Is called when the window closes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Closing(object sender, CancelEventArgs e)
+        {
+            if (!hasReturned)
+                ReturnData(null);
         }
     }
 }

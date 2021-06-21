a4279073f4556375281a2901642bd04dd1b5cc94
David Huemer <92david.huemer@gmail.com>
2021-06-21 16:35:34 +0200

    Added basic MenuPanel

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/MenuPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels
+{
+    /// <summary>
+    /// Interaktionslogik für MenuPanel.xaml
+    /// </summary>
+    public partial class MenuPanel : UserControl
+    {
+        public MenuPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

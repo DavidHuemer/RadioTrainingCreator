f71a4e9d7ccb580dc5a25e821fd300365de69802
David Huemer <92david.huemer@gmail.com>
2021-06-21 12:18:47 +0200

    Added FireDepartment

---
+++ RadioTrainingCreator.Data/FireDepartment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RadioTrainingCreator.Data
+{
+    [Serializable]
+    public class FireDepartment
+    {
+        public string FireDepartmentType { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+}

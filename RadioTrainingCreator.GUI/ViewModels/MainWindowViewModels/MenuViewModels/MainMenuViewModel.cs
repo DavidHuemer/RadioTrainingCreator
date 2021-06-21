0a90fbbd5a3dd8be92fde1015099b7045eecb834
David Huemer <92david.huemer@gmail.com>
2021-06-21 16:34:53 +0200

    Added NewProject RelayCommand

--- RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/MenuViewModels/MainMenuViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/MenuViewModels/MainMenuViewModel.cs
@@ -1,9 +1,24 @@
-﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+﻿using MVVM.Tools;
+using RadioTrainingCreator.GUI.ViewModels.Basics;
+using System;
 
 namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.MenuViewModels
 {
     public class MainMenuViewModel : BaseViewModel
     {
+        public MainMenuViewModel()
+        {
 
+        }
+
+        public RelayCommand<string> NewProject => new RelayCommand<string>(x =>
+        {
+            DoNewProject();
+        }, x => true);
+
+        public void DoNewProject()
+        {
+            Console.WriteLine("New Project clicked");
+        }
     }
 }

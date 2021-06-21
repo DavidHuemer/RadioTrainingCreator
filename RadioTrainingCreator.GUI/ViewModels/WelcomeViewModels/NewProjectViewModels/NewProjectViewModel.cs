dfb57e216dfb764d055aab154951197085d4be89
David Huemer <92david.huemer@gmail.com>
2021-06-21 20:59:20 +0200

    Moved most of the properties to an extra ViewModel called NewProjectDataViewModel.

--- RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/NewProjectViewModels/NewProjectViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/NewProjectViewModels/NewProjectViewModel.cs
@@ -1,52 +1,24 @@
-﻿using MVVM.Tools;
-using RadioTrainingCreator.Data;
+﻿using RadioTrainingCreator.Data.Files;
 using RadioTrainingCreator.GUI.Services.Interfaces.FileInterfaces;
 using RadioTrainingCreator.GUI.ViewModels.Basics;
 using RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels;
-using RadioTrainingCreator.Handler.FilesHandler;
 using System;
 
 namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewModels
 {
     public class NewProjectViewModel : BaseViewModel
     {
-        #region Properties
-
-        public string ProjectName
-        {
-            get => projectName; set
-            {
-                projectName = value;
-                UpdateFullPath();
-            }
-        }
-        public string ProjectFolder
-        {
-            get => projectFolder; set
-            {
-                projectFolder = value;
-                UpdateFullPath();
-            }
-        }
-        public string FullPath { get; set; } = "";
-        public string Author { get; set; } = "";
-        public string Comment { get; set; } = "";
-
-        #endregion
-
-        #region Private variables
+        public NewProjectDataViewModel DataViewModel { get; set; }
 
         private readonly IFileDialogService fileDialogService;
-        private string projectName = "";
-        private string projectFolder = "";
 
         private Action projectCreated;
-
-        #endregion
 
         public NewProjectViewModel(IFileDialogService fileDialogService)
         {
             this.fileDialogService = fileDialogService;
+            DataViewModel = new NewProjectDataViewModel(fileDialogService);
+            DataViewModel.Init(ProjectCreated);
         }
 
         public NewProjectViewModel()
@@ -59,56 +31,10 @@ namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels.NewProjectViewMo
             this.projectCreated = projectCreated;
         }
 
-        #region Commands
-
-        #region ChooseFolder
-
-        public RelayCommand<string> ChooseFolder => new RelayCommand<string>(x =>
+        public void ProjectCreated(CreatedRadioTraining createdRadioTraining)
         {
-            DoChooseFolder();
-        }, x => true);
-        public void DoChooseFolder()
-        {
-            Console.WriteLine("Choose folder");
-            ProjectFolder = fileDialogService.GetFolder();
-        }
-
-        #endregion
-
-        #region CreateRadioTraining
-
-        public RelayCommand<string> CreateRadioTraining => new RelayCommand<string>(x =>
-        {
-            DoCreateRadioTraining();
-        }, x => true);
-
-        public void DoCreateRadioTraining()
-        {
-            Console.WriteLine("Create Radio Training");
-            var createdRadioTraining = RadioTrainingProjectHandler
-                .CreateRadioTraining(ProjectFolder, ProjectName, Author, Comment);
-
-            //CurrentOpenedProject.Instance.Init(createdRadioTraining);
             MainWindowViewModel.Instance.Open(createdRadioTraining.FilePath, createdRadioTraining.RadioTraining);
-
             projectCreated?.Invoke();
         }
-
-        #endregion
-
-        #endregion
-
-        #region UpdateFullPath
-
-        /// <summary>
-        /// Updates the full path
-        /// </summary>
-        private void UpdateFullPath()
-        {
-            var path = FilePathHandler.CombineRadioTrainingPath(ProjectFolder, ProjectName);
-            FullPath = path;
-        }
-
-        #endregion
     }
 }

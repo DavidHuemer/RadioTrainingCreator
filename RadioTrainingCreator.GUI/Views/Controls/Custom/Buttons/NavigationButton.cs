d873c0e0d356d7ffadc3687e25009db19303d802
David Huemer <92david.huemer@gmail.com>
2021-06-22 15:19:41 +0200

    Added NavigationButton

---
+++ RadioTrainingCreator.GUI/Views/Controls/Custom/Buttons/NavigationButton.cs
@@ -0,0 +1,21 @@
+﻿using RadioTrainingCreator.GUI.Views.Controls.Default;
+using System.Windows;
+
+namespace RadioTrainingCreator.GUI.Views.Controls.Custom.Buttons
+{
+    public class NavigationButton : ExButton
+    {
+        #region Is Selected
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register(
+          nameof(IsSelected), typeof(bool), typeof(NavigationButton), new PropertyMetadata(false));
+
+        #endregion
+    }
+}

c51bf0578bbe51fddcc07a6fdd7d3e6b03469114
David Huemer <92david.huemer@gmail.com>
2021-06-22 15:39:03 +0200

    Added NavigationViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/NavigationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels
+{
+    /// <summary>
+    /// Handles the navigation between the different pages
+    /// </summary>
+    public class NavigationViewModel : BaseViewModel
+    {
+        public NavigationViewModel()
+        {
+
+        }
+    }
+}

7af916fb8067b73aa4d71f2a0145ddadf1b758d4
David Huemer <92david.huemer@gmail.com>
2021-06-22 11:34:58 +0200

    Added NewProjectCreated method. Added windowService and filedialogService support

--- RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/MenuViewModels/MainMenuViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/MenuViewModels/MainMenuViewModel.cs
@@ -1,24 +1,49 @@
 ﻿using MVVM.Tools;
+using RadioTrainingCreator.Data.Files;
+using RadioTrainingCreator.GUI.Services.FileServices;
+using RadioTrainingCreator.GUI.Services.Interfaces;
+using RadioTrainingCreator.GUI.Services.Interfaces.FileInterfaces;
+using RadioTrainingCreator.GUI.Services.Services.WindowServices;
 using RadioTrainingCreator.GUI.ViewModels.Basics;
+using RadioTrainingCreator.GUI.ViewModels.DataWindowsViewModels;
 using System;
 
 namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.MenuViewModels
 {
     public class MainMenuViewModel : BaseViewModel
     {
-        public MainMenuViewModel()
+        private readonly IWindowService windowService;
+        private readonly IFileDialogService fileDialogService;
+
+        public MainMenuViewModel(IFileDialogService fileDialogService)
+        {
+            windowService = WindowServiceHandler.GetCorrectWindowService();
+            this.fileDialogService = fileDialogService;
+        }
+
+        public MainMenuViewModel() : this(new FileDialogService())
         {
 
         }
 
         public RelayCommand<string> NewProject => new RelayCommand<string>(x =>
         {
-            DoNewProject();
+            DoCreateNewProject();
         }, x => true);
 
-        public void DoNewProject()
+        public void DoCreateNewProject()
         {
             Console.WriteLine("New Project clicked");
+            var newProjectVm = new RadioTrainingDataWindowViewModel(fileDialogService);
+            newProjectVm.Init(NewProjectCreated);
+            windowService.Open(newProjectVm);
+        }
+
+        private void NewProjectCreated(CreatedRadioTraining data)
+        {
+            if (data == null)
+                return;
+            MainWindowViewModel.Instance.Open(data.FilePath, data.RadioTraining);
         }
     }
 }

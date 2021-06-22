5ad8e40918183df1f55b7903960994371824f9ca
David Huemer <92david.huemer@gmail.com>
2021-06-22 15:35:01 +0200

    Added PageViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/PageViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+{
+    /// <summary>
+    /// Base class for all PageViewModels
+    /// </summary>
+    public class PageViewModel : BaseViewModel
+    {
+        public PageViewModel(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public string DisplayName { get; set; } = "";
+    }
+}

01f3b4aed866b51c7b2ee8c451300444bc35e1e7
David Huemer <92david.huemer@gmail.com>
2021-06-22 15:05:34 +0200

    Added ContentPanel

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/ContentPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels
+{
+    /// <summary>
+    /// Interaktionslogik für ContentPanel.xaml
+    /// </summary>
+    public partial class ContentPanel : UserControl
+    {
+        public ContentPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

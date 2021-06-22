06f1e78bebc5f8cb755b182cce5f4023a875cbbd
David Huemer <92david.huemer@gmail.com>
2021-06-22 15:42:52 +0200

    Added FireDepartmentsPageViewModel and LocationsPageViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/FireDepartments/FireDepartmentsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.FireDepartments
+{
+    public class FireDepartmentsPageViewModel : PageViewModel
+    {
+        public FireDepartmentsPageViewModel() : base("Feuerwehren")
+        {
+
+        }
+    }
+}
---
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/Locations/LocationsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+
+namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.Locations
+{
+    public class LocationsPageViewModel : PageViewModel
+    {
+        public LocationsPageViewModel() : base("Koordinaten")
+        {
+
+        }
+    }
+}

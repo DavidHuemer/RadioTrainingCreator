84af22083b2edf1164c26b685fce2042954091fa
David Huemer <92david.huemer@gmail.com>
2021-06-23 18:36:06 +0200

    Added CurrentRadioTraining to the PageViewModel.

--- RadioTrainingCreator.GUI/ViewModels/Basics/PageViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/Basics/PageViewModel.cs
@@ -1,13 +1,18 @@
-﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+﻿using RadioTrainingCreator.Data;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
 {
     /// <summary>
     /// Base class for all PageViewModels
     /// </summary>
     public class PageViewModel : BaseViewModel
     {
+        public RadioTraining CurrentRadioTraining { get; set; }
+
         public PageViewModel(string displayName)
         {
             DisplayName = displayName;
+            CurrentRadioTraining = CurrentOpenedProject.Instance.RadioTraining;
         }
 
         public string DisplayName { get; set; } = "";

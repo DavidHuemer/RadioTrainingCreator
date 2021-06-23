22dd91b0ec9471446c70be7af1012cf61a4bfacd
David Huemer <92david.huemer@gmail.com>
2021-06-23 18:59:01 +0200

    Added FireDepartmentEditor.xaml

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/FireDepartmentPanels/FireDepartmentEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels.FireDepartmentPanels
+{
+    /// <summary>
+    /// Interaktionslogik für FireDepartmentEditor.xaml
+    /// </summary>
+    public partial class FireDepartmentEditor : UserControl
+    {
+        public FireDepartmentEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}

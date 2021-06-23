225364b7c4a2931e8873bccefd47a20ace64f15c
David Huemer <92david.huemer@gmail.com>
2021-06-23 17:00:08 +0200

    Added NavigationPanel

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/NavigationPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels
+{
+    /// <summary>
+    /// Interaktionslogik für NavigationPanel.xaml
+    /// </summary>
+    public partial class NavigationPanel : UserControl
+    {
+        public NavigationPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

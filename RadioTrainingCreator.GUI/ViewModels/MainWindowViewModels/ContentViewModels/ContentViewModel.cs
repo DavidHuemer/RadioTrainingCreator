5a7683ce41e45770d1021721c4bdc5c4fedca541
David Huemer <92david.huemer@gmail.com>
2021-06-23 13:21:47 +0200

    Set default value for the PageViewModels.
    Added ChangePage RelayCommand.

--- RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/ContentViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/ContentViewModel.cs
@@ -1,6 +1,8 @@
-﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+﻿using MVVM.Tools;
+using RadioTrainingCreator.GUI.ViewModels.Basics;
 using RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.FireDepartments;
 using RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.Locations;
+using System;
 
 namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels
 {
@@ -9,13 +11,23 @@ namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewMo
     /// </summary>
     public class ContentViewModel : BaseViewModel
     {
-        public FireDepartmentsPageViewModel FireDepartmentsPage { get; set; }
-        public LocationsPageViewModel LocationsPage { get; set; }
+        public FireDepartmentsPageViewModel FireDepartmentsPage { get; set; } = null;
+        public LocationsPageViewModel LocationsPage { get; set; } = null;
+        public PageViewModel CurrentPage { get; set; } = null;
 
         public ContentViewModel()
         {
             FireDepartmentsPage = new FireDepartmentsPageViewModel();
             LocationsPage = new LocationsPageViewModel();
+            CurrentPage = FireDepartmentsPage;
+        }
+
+        public RelayCommand<PageViewModel> ChangePage => new RelayCommand<PageViewModel>(o => { DoChangePage(o); }, o => true);
+
+        private void DoChangePage(PageViewModel o)
+        {
+            Console.WriteLine($"Change current page to {o.DisplayName}");
+            CurrentPage = o;
         }
     }
 }

96d162f6a419f79eb6a244b7074b6d51545f1616
David Huemer <92david.huemer@gmail.com>
2021-06-23 18:43:41 +0200

    Added FireDepartments and Florian in the RadioTraining

--- RadioTrainingCreator.Data/RadioTraining.cs
+++ RadioTrainingCreator.Data/RadioTraining.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 
 namespace RadioTrainingCreator.Data
 {
@@ -8,5 +9,8 @@ namespace RadioTrainingCreator.Data
         public string Name { get; set; } = "";
         public string Author { get; set; } = "";
         public string Comment { get; set; } = "";
+
+        public ObservableCollection<FireDepartment> FireDepartments { get; set; } = new ObservableCollection<FireDepartment>();
+        public FireDepartment Florian { get; set; } = null;
     }
 }

8b052e7e19c1b49edb254c6c759a6a73b5906e75
David Huemer <92david.huemer@gmail.com>
2021-06-23 18:47:27 +0200

    Added EditorViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+{
+    /// <summary>
+    /// Base class for all EditorViewModels
+    /// </summary>
+    public abstract class EditorViewModel : BaseViewModel
+    {
+
+    }
+}

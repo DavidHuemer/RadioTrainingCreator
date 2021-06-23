f569e4ceeb740c5cfc50bb0b2d86d8d33d39fad4
David Huemer <92david.huemer@gmail.com>
2021-06-23 13:36:59 +0200

    Added LocationsPanel

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/LocationPanels/LocationsPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels.LocationPanels
+{
+    /// <summary>
+    /// Interaktionslogik für LocationsPanel.xaml
+    /// </summary>
+    public partial class LocationsPanel : UserControl
+    {
+        public LocationsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

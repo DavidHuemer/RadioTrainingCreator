c936fd9ea546318b8ea60e9fd3a375fc3c029866
David Huemer <92david.huemer@gmail.com>
2021-06-24 12:39:14 +0200

    Added generic type to the EditorViewModel.

--- RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModel.cs
@@ -1,11 +1,16 @@
-﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics
+﻿using MVVM.Tools;
+using System;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
 {
     /// <summary>
     /// Base class for all EditorViewModels
     /// </summary>
-    public abstract class EditorViewModel : BaseViewModel
+    public abstract class EditorViewModel<T> : BaseViewModel where T : class
     {
         public bool IsVisible { get; set; } = false;
+
+        public T CurrentObject { get; set; } = null;
 
         /// <summary>
         /// Should be called when the editor should create a new "item"
--- RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/FireDepartments/FireDepartmentEditorViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/FireDepartments/FireDepartmentEditorViewModel.cs
@@ -1,8 +1,9 @@
-﻿using RadioTrainingCreator.GUI.ViewModels.Basics;
+﻿using RadioTrainingCreator.Data;
+using RadioTrainingCreator.GUI.ViewModels.Basics;
 
 namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.FireDepartments
 {
-    public class FireDepartmentEditorViewModel : EditorViewModel
+    public class FireDepartmentEditorViewModel : EditorViewModel<FireDepartment>
     {
         public string RadioCallName { get; set; } = "";
         public string Name { get; set; } = "";

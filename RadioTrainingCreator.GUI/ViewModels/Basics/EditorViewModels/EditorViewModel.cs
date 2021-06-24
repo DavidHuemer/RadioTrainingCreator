b35ad2cbf606c039e81e2b501acfd2a13d083bf1
David Huemer <92david.huemer@gmail.com>
2021-06-24 14:08:14 +0200

    Added custom EditorViewModels folder. Added RadioTrainingEditorViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModels/EditorViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MVVM.Tools;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics.EditorViewModels
+{
+    /// <summary>
+    /// Base class for all EditorViewModels
+    /// </summary>
+    public abstract class EditorViewModel : BaseViewModel
+    {
+        public bool IsVisible { get; set; } = false;
+
+        /// <summary>
+        /// Should be called when the editor should create a new "item"
+        /// </summary>
+        public virtual void Add()
+        {
+            Clear();
+            IsVisible = true;
+        }
+
+        /// <summary>
+        /// Clears the properties
+        /// </summary>
+        public abstract void Clear();
+
+        #region Commands
+
+        #region Save
+
+        public RelayCommand<string> Save => new RelayCommand<string>(o => { DoSave(); }, o => true);
+
+        protected abstract void DoSave();
+
+        #endregion
+
+        protected abstract void CreateNew();
+        protected abstract void UpdateCurrent();
+
+        #endregion
+    }
+}
---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModels/RadioTrainingEditorViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics.EditorViewModels
+{
+    public abstract class RadioTrainingEditorViewModel<T> : EditorViewModel where T : class
+    {
+        public T CurrentObject { get; set; } = null;
+
+        protected override void DoSave()
+        {
+            if (CurrentObject == null)
+            {
+                CreateNew();
+            }
+            else
+            {
+                UpdateCurrent();
+            }
+        }
+    }
+}

b75d64e8319c147f097fe99cf15637b26247b7f0
David Huemer <92david.huemer@gmail.com>
2021-06-24 12:06:43 +0200

    Added EditorPanel.

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/EditorPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels
+{
+    /// <summary>
+    /// Interaktionslogik für EditorPanel.xaml
+    /// </summary>
+    public partial class EditorPanel : UserControl
+    {
+        public EditorPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}

f94c9dd45d07a57b95682375b6629e32630c7837
David Huemer <92david.huemer@gmail.com>
2021-06-24 16:13:53 +0200

    Added CloseAbleViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/CloseAbleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RadioTrainingCreator.GUI.ViewModels.Basics
+{
+    public class CloseAbleViewModel : BaseViewModel
+    {
+        private Action closeCallback;
+
+        /// <summary>
+        /// Inits the close callback
+        /// </summary>
+        /// <param name="closeCallback">The callback that should be called when the viewmodel closes</param>
+        public void InitClose(Action closeCallback)
+        {
+            this.closeCallback = closeCallback;
+        }
+
+        /// <summary>
+        /// Calls the close callback
+        /// </summary>
+        public void Close()
+        {
+            closeCallback?.Invoke();
+        }
+    }
+}

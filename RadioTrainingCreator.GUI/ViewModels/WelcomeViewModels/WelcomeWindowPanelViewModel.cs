adb06e0e3cefa76d9f57f480129cc8df3020dd56
David Huemer <92david.huemer@gmail.com>
2021-06-27 16:08:31 +0200

    Added OpenProject callback.

--- RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowPanelViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/WelcomeViewModels/WelcomeWindowPanelViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RadioTrainingCreator.Data;
+using RadioTrainingCreator.GUI.ViewModels.Basics;
+using System;
 
 namespace RadioTrainingCreator.GUI.ViewModels.WelcomeViewModels
 {
-    public class WelcomeWindowPanelViewModel
+    public class WelcomeWindowPanelViewModel : BaseViewModel
     {
+        private Action<string, RadioTraining> openProjectCallback;
+
+        /// <summary>
+        /// Inits the project created callback
+        /// </summary>
+        /// <param name="closeCallback">The callback that should be called when the viewmodel wants to open a project</param>
+        public void InitOpenProject(Action<string, RadioTraining> closeCallback)
+        {
+            this.openProjectCallback = closeCallback;
+        }
+
+        /// <summary>
+        /// Calls the open project callback
+        /// </summary>
+        public void OpenProject(string filePath, RadioTraining radioTraining)
+        {
+            openProjectCallback?.Invoke(filePath, radioTraining);
+        }
     }
 }

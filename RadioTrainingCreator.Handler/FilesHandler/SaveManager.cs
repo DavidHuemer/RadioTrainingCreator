1b4c0834febc990d1a610915cbc5f6072589dfbc
David Huemer <92david.huemer@gmail.com>
2021-06-28 15:33:56 +0200

    Added SaveManager.

---
+++ RadioTrainingCreator.Handler/FilesHandler/SaveManager.cs
@@ -0,0 +1,26 @@
+﻿namespace RadioTrainingCreator.Handler.FilesHandler
+{
+    public class SaveManager
+    {
+        #region Singleton
+
+        private static SaveManager instance;
+        public static SaveManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SaveManager();
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        private SaveManager()
+        {
+
+        }
+    }
+}

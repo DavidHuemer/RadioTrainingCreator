ef794fa927952b04ab292e5e58b1912252e14619
David Huemer <92david.huemer@gmail.com>
2021-06-28 14:50:28 +0200

    Added StringExtensions.

---
+++ RadioTrainingCreator.Data/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace RadioTrainingCreator.Data.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks if a string of the values is empty
+        /// </summary>
+        /// <param name="values">The string params that should be checked</param>
+        /// <returns>Returns if a string of the params is empty</returns>
+        public static bool AreEmpty(params string[] values)
+        {
+            foreach (var item in values)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}

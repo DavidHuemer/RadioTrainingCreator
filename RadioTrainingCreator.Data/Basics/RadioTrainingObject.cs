db4e585ef70ad706cb52fbde664412ac87447b50
David Huemer <92david.huemer@gmail.com>
2021-06-29 14:34:59 +0200

    Added RadioTrainingObject.

---
+++ RadioTrainingCreator.Data/Basics/RadioTrainingObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace RadioTrainingCreator.Data.Basics
+{
+    /// <summary>
+    /// Base class for all RadioTraining objects
+    /// </summary>
+    [Serializable]
+    public class RadioTrainingObject : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}

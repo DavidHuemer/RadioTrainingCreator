85c575e6db6cbbb4b5dba75f8806672ca54c153d
David Huemer <92david.huemer@gmail.com>
2021-06-29 15:43:03 +0200

    Added LocationEditor.

---
+++ RadioTrainingCreator.GUI/Views/Panels/MainWindowPanels/ContentPanels/LocationPanels/LocationEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadioTrainingCreator.GUI.Views.Panels.MainWindowPanels.ContentPanels.LocationPanels
+{
+    /// <summary>
+    /// Interaktionslogik für LocationEditor.xaml
+    /// </summary>
+    public partial class LocationEditor : UserControl
+    {
+        public LocationEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}

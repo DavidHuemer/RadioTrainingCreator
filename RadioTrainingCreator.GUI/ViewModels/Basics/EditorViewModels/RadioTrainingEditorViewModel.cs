3e35cf619131aba2beb814a69fe1bf4106a2b9c3
David Huemer <92david.huemer@gmail.com>
2021-06-29 14:43:59 +0200

    Added ShowUpdateItem method. Added SetProperties method.

--- RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModels/RadioTrainingEditorViewModel.cs
+++ RadioTrainingCreator.GUI/ViewModels/Basics/EditorViewModels/RadioTrainingEditorViewModel.cs
@@ -2,9 +2,24 @@
 
 namespace RadioTrainingCreator.GUI.ViewModels.Basics.EditorViewModels
 {
+    /// <summary>
+    /// Base class for all editors that edits a RadioTrainingObject
+    /// </summary>
+    /// <typeparam name="T">The type of the RadioTrainingObject</typeparam>
     public abstract class RadioTrainingEditorViewModel<T> : EditorViewModel where T : class
     {
         public T CurrentObject { get; set; } = null;
+
+        /// <summary>
+        /// Should be called when the editor should update an "item"
+        /// </summary>
+        public void ShowUpdateItem(T item)
+        {
+            IsVisible = true;
+            CurrentObject = item;
+            Clear();
+            SetProperties(item);
+        }
 
         protected override void DoSave()
         {
@@ -17,7 +32,14 @@ namespace RadioTrainingCreator.GUI.ViewModels.Basics.EditorViewModels
                 UpdateCurrent();
             }
 
+            Hide();
             SaveManager.Instance.Unsave();
         }
+
+        /// <summary>
+        /// Sets the properties of the item
+        /// </summary>
+        /// <param name="item">The item which properties should be copied</param>
+        protected abstract void SetProperties(T item);
     }
 }

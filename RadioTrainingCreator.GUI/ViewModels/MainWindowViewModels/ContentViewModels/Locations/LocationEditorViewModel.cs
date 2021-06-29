4a41c9ba1c1388b82ef02c430254b69aa2b88e60
David Huemer <92david.huemer@gmail.com>
2021-06-29 15:39:27 +0200

    Added LocationEditorViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/MainWindowViewModels/ContentViewModels/Locations/LocationEditorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using RadioTrainingCreator.Data.RadioTrainingObjects;
+using RadioTrainingCreator.GUI.ViewModels.Basics.EditorViewModels;
+
+namespace RadioTrainingCreator.GUI.ViewModels.MainWindowViewModels.ContentViewModels.Locations
+{
+    public class LocationEditorViewModel : RadioTrainingEditorViewModel<Location>
+    {
+        public override void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CreateNew()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void SetProperties(Location item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateCanSave()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateCurrent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

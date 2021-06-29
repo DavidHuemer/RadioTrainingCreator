9da505df567738ef08549473247f41aa7c9cdb84
David Huemer <92david.huemer@gmail.com>
2021-06-29 14:44:14 +0200

    Added RadioTrainingItemPageViewModel.

---
+++ RadioTrainingCreator.GUI/ViewModels/Basics/PageViewModels/RadioTrainingItemPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace RadioTrainingCreator.GUI.ViewModels.Basics.PageViewModels
+{
+    public abstract class RadioTrainingItemPageViewModel<T> : PageViewModel where T : class
+    {
+        private T item;
+        public T CurrentItem
+        {
+            get { return item; }
+            set
+            {
+                item = value;
+                if (item != null)
+                    ShowUpdateCurrent(value);
+            }
+        }
+
+        public RadioTrainingItemPageViewModel(string displayName) : base(displayName)
+        {
+            
+        }
+
+        public abstract void ShowUpdateCurrent(T item);
+    }
+}
